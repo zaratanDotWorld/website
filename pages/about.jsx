4e357ccd98f167935c7d68ce0234fe80c7ff9ef8
Daniel Kronovet <kronovet@gmail.com>
2025-01-18 09:30:48 -0500

    Rework /about

--- pages/about.jsx
+++ pages/about.jsx
@@ -60,11 +60,11 @@ export default function () {
               <ul>
                 <li>the human need for connection</li>
                 <li>the rising cost of housing</li>
-                <li>the decline of democratic civil society</li>
+                <li>the decline of civil society</li>
               </ul>
 
               <p>
-                We see these challenges as linked, and believe that <b>coliving can address them all</b>, by enabling residents to co-create their own housing:
+                We see these challenges as linked, and believe that <b>coliving can address them all</b>, by enabling residents to co-create their own housing —
                 building relationships, saving money, and cultivating skills in the process.
               </p>
 
@@ -89,9 +89,9 @@ export default function () {
                 <i>What is a Zaratan?</i>
               </p>
               <p>
-                <Link href={zaratanUrl}>Zaratani</Link> are <b>mythical giant sea turtles</b>, often mistaken for islands by sailors lost at sea.
-                By naming ourselves after these creatures, we evoke the way that organizational innovations can act as foundations for thriving communities.
-                In addition, the Zaratan is known for its slow movement and long life, which we evoke through our intentional approach to growth.
+                A <Link href={zaratanUrl}>Zaratan</Link> is a <b>mythical giant sea turtle</b>, often mistaken for an island by sailors lost at sea.
+                By naming ourselves after this creature, we evoke the way that organizational innovations can act as foundations for thriving communities.
+                In addition, Zaratani are known for their slow movement and long life, which we evoke through our intentional approach to growth.
               </p>
             </Col>
           </Row>
@@ -102,19 +102,20 @@ export default function () {
             <Col xl={7}>
               <h5><b>Our Story</b></h5>
               <p>
-                In his ten years working across the fields of communal living, organizational design, and technology development, Zaratan founder Daniel Kronovet developed the concept for a certain kind of house
+                Over a ten-year career working across the fields of communal living, organizational design, and technology development, Zaratan founder Daniel Kronovet developed the concept for a certain kind of house
                 — one that was <b>accessible, appealing, and easy to live in</b>.
-                In March of 2020, Daniel moved back to his hometown of Los Angeles to try and make this dream a reality.
               </p>
               <p>
+                In March of 2020, Daniel moved back to his hometown of Los Angeles to try and make this dream a reality.
                 Within days, the pandemic hit.
                 Undeterred, Daniel teamed up with his dad, Robert Kronovet, recruited architect Matt Manion, and started working on what would become Sage House.
                 Over a period of two years they renovated what was then a run-down old house into a beautiful and functional new home.
                 At the same time, Daniel worked with collaborators Seth Frey and Joseph DeSimone to develop Chore Wheel, the tool that would help Sage function.
               </p>
               <p>
-                In September 2022, Sage opened and filled up quickly.
-                As an early resident of the house, Daniel helped build a culture of practice around Chore Wheel, one which could withstand the effects of turnover and interpersonal frictions while supporting resident's agency and personal growth.
+                Sage opened in September 2022, filling up quickly.
+                An early resident of the house, Daniel helped build a culture of practice around Chore Wheel —
+                one which could withstand the effects of turnover and interpersonal friction while supporting the residents' agency and personal growth.
               </p>
               <p>
                 After a year at Sage, Daniel moved out to focus on increasing the adoption of Chore Wheel by the larger housing ecosystem — where it is being actively used.
@@ -149,19 +150,19 @@ export default function () {
             <Col xl={7}>
               <h5><b>About Daniel</b></h5>
               <p>
-                Daniel was born and raised in Santa Monica, just down the road from what would eventually become Sage House.
+                Daniel was born and raised in Santa Monica, down the road from what would eventually become Sage House.
               </p>
               <p>
-                A passionate student, Daniel studied at UC Berkeley, where he pursued a double-major in Political Economy and Cognitive Science.
-                While at Cal, Daniel had his first experience of communal living as a member of the <Link href="https://bsc.coop">Berkeley Student Cooperative</Link>.
-                Connecting deeply with the co-op's mission, Daniel got involved in leadership, eventually becoming president — combining his intellectual development with a rich experience of practice.
+                An energetic student, Daniel studied at UC Berkeley, pursuing a double-major in Political Economy and Cognitive Science.
+                While at Cal, Daniel had his first experience of communal living through the <Link href="https://bsc.coop">Berkeley Student Cooperative</Link>.
+                Connecting deeply with the co-op's mission, Daniel got involved in leadership, eventually becoming president — complementing his intellectual development with a rich experience of practice.
               </p>
               <p>
                 After graduating, Daniel spent a year abroad, with extended stays on a kibbutz in the north of Israel, a yeshiva in Jerusalem, and a monastery in Thailand.
                 These experiences broadened his perspective, exposing him to a diversity of organizational structures and practices.
               </p>
               <p>
-                Returning to the United States, Daniel moved to New York City and went into technology, earning a master's degree in Quantitative Social Science at Columbia University, with <Link href={thesisUrl}>research focused on voting systems</Link>.
+                Returning to the United States, Daniel went to New York City and into technology, earning a master's degree in Quantitative Social Science at Columbia University, <Link href={thesisUrl}>researching voting systems</Link>.
                 Starting his career as a machine learning engineer, he soon pivoted to mechanism design, as a research engineer at the pioneering future-of-work project <Link href="https://colony.io">Colony</Link> (where he still actively contributes).
                 In this role, Daniel gained a deeper understanding of the skills needed to design and implement complex systems.
               </p>
@@ -172,7 +173,7 @@ export default function () {
               </p>
 
               <p>
-                <b>Want to know more?</b> <Link href="mailto:hello@zaratan.world">Drop us a line</Link>
+                <b>Want to know more?</b> <Link href="mailto:hello@zaratan.world">Drop us a line</Link>.
               </p>
             </Col>
           </Row>
