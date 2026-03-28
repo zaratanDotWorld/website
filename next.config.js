d26503abecc55d17d98d8065ac7d3f83e0225ce7
Daniel Kronovet <kronovet@gmail.com>
2026-03-28 16:35:46 -0400

    feat: add useful redirects

--- next.config.js
+++ next.config.js
@@ -5,4 +5,23 @@ module.exports = {
       { protocol: 'https', hostname: 'd3gacl6pm59h8m.cloudfront.net'}
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/apply',
+        destination: 'https://zaratan.managebuilding.com/Resident/rental-application/new',
+        permanent: true,
+      },
+      {
+        source: '/sage',
+        destination: '/houses/sage',
+        permanent: true,
+      },
+      {
+        source: '/interest',
+        destination: 'https://tally.so/r/gDLPzN',
+        permanent: true,
+      },
+    ];
+  },
 }
